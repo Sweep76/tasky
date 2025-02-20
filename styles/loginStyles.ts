eceeaba0613a1f856b23a75390f876d83eb9e381
Sweep <joshuapatrickchiu76@gmail.com>
2025-02-20 20:47:05 +0800

    style: login stylization

---
+++ styles/loginStyles.ts
@@ -0,0 +1,60 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    fontSize: 16,
+    marginBottom: 15,
+  },
+  button: {
+    backgroundColor: "#3498db",
+    width: "100%",
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 10,
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  forgotPassword: {
+    marginTop: 15,
+    color: "#3498db",
+    fontSize: 14,
+  },
+  signupContainer: {
+    flexDirection: "row",
+    marginTop: 20,
+  },
+  signupText: {
+    fontSize: 14,
+    color: "#555",
+  },
+  signupLink: {
+    fontSize: 14,
+    color: "#3498db",
+    fontWeight: "bold",
+  },
+});
+
+export default styles;
